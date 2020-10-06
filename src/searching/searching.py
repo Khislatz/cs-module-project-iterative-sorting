a31790d84ac9308bcb2c88962d8765517cf3bb90
Khislatz <47505600+Khislatz@users.noreply.github.com>
2020-10-05 20:13:03 -0400

    searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,36 @@
+# return the index at which the number is located if True, and return -1 is False  
+
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(len(arr)): 
+  
+        if arr[i] == target: 
+            return i 
+  
     return -1   # not found
 
-
 # Write an iterative implementation of Binary Search
+# return the index at which the number is located if True, and return -1 is False   
 def binary_search(arr, target):
+    arr.sort()
+    start = 0
+    end = len(arr)-1
 
-    # Your code here
+    while end >= start:
+        # get the middle point
+        # if the middle value is the same as target, set found to True
+        middle_index = (start + end) // 2
+        if arr[middle_index] == target:
+            return middle_index
+        # move start or end index closer to one another, and shrink our search space     
+        else:
+            if target < arr[middle_index]:
+                end = middle_index - 1
+            if target > arr[middle_index]:
+                start = middle_index + 1
+    return -1
 
+arr = [1, 13, 14, 22, 45, 12]
 
-    return -1  # not found
+print(linear_search(arr, 14))
+print(binary_search(arr, 45))
