0f78d97bc2abe12e8c4cc37412be71c847c524e9
Khislatz <47505600+Khislatz@users.noreply.github.com>
2020-10-05 20:11:49 -0400

    iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,20 +7,37 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        for j in range(smallest_index, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
         # Your code here
-
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
+print(selection_sort([12, 34, 45, 24]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    n = len(arr)
+
+    # Loop through n-1 elements
+    for i in range(n-1):
+        # range(n) also works but outer loop will repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
     return arr
+
+arr1 = [64, 34, 25, 12, 22, 11, 90] 
+  
+print(bubble_sort(arr1))
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +56,23 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+def counting_sort(array, maxval):
+    """in-place counting sort"""
+    n = len(array)
+    m = maxval + 1
+    # init with zeros
+    count = [0] * m               
+    for a in array:
+        # count occurences
+        count[a] += 1             
+    i = 0
+    for a in range(m):
+        # - emit 'count[a]' copies of 'a'        
+        for c in range(count[a]): 
+            array[i] = a
+            i += 1
+    return array
 
-
-    return arr
+arr2 = [1, 2, 2, 9, 7, 3] 
+  
+print(counting_sort(arr2, 9))
